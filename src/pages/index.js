626d3a43a492943e630336349111567136b035e6
Michael Verdi <michael.v.verdi@gmail.com>
2019-10-16 13:47:42 -0400

    added basic form

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,27 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>We want to know what you think!</h1>
+    <form name="contact" data-netlify="true">
+      <p>
+        <label>
+          Name <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>What topics are most important to you</label>
+        <br />
+        <select>
+          <option value="economy">Economy</option>
+          <option value="health care">Health Care</option>
+          <option value="education">Education</option>
+          <option value="crime">Crime</option>
+        </select>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
