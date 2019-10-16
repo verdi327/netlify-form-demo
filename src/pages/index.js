0a698de7a8c9b24f90ff64622dec971d0e0dff03
Michael Verdi <michael.v.verdi@gmail.com>
2019-10-16 13:56:30 -0400

    adding more to form

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,25 +9,34 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>We want to know what you think!</h1>
-    <form name="contact" data-netlify="true">
-      <p>
-        <label>
-          Name <input type="text" name="name" />
-        </label>
-      </p>
-      <p>
-        <label>What topics are most important to you</label>
-        <br />
-        <select>
-          <option value="economy">Economy</option>
-          <option value="health care">Health Care</option>
-          <option value="education">Education</option>
-          <option value="crime">Crime</option>
-        </select>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
+    <form
+      name="contact"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input type="hidden" name="bot-field" />
+      <input type="hidden" name="form-name" value="contact" />
+      <div className="field half first">
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+      </div>
+      <div className="field half">
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email" />
+      </div>
+      <div className="field">
+        <label htmlFor="message">Message</label>
+        <textarea name="message" id="message" rows="6" />
+      </div>
+      <ul className="actions">
+        <li>
+          <input type="submit" value="Send Message" className="special" />
+        </li>
+        <li>
+          <input type="reset" value="Clear" />
+        </li>
+      </ul>
     </form>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
